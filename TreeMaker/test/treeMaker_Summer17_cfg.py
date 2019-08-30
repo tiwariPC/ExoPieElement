3c2cc073034170b2abb02b12ff77f829b1dfe32a
Praveen Chandra Tiwari <tiwariPC@users.noreply.github.com>
2019-08-30 17:49:29 +0530

    minor

--- TreeMaker/test/treeMaker_Summer17_cfg.py
+++ TreeMaker/test/treeMaker_Summer17_cfg.py
@@ -109,7 +109,6 @@ elif options.runOn2016:
 
 process.source = cms.Source("PoolSource",
                             secondaryFileNames = cms.untracked.vstring(),
-
                             fileNames = cms.untracked.vstring("file:/tmp/khurana/temp.root"),
 			    #fileNames = cms.untracked.vstring(testFile),
 			    #skipEvents = cms.untracked.uint32(0)
