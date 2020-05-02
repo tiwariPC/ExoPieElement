f8dc1423522b6453b052a7ccf0b29d807416b8c9
Raman Khurana <raman.khurana@cern.ch>
2020-05-02 08:54:04 -0500

    updated commit tag to setup_2017_2016_v03

--- CrabUtilities/crabConfig_2017_MC.py
+++ CrabUtilities/crabConfig_2017_MC.py
@@ -1,7 +1,7 @@
 from WMCore.Configuration import Configuration
 config = Configuration()
 
-workname ='setup_2017_2016_v01'
+workname ='setup_2017_2016_v03'
 dataset='/DYJetsToLL_M-50_HT-400to600_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM' ## this is temporrt, will be changed by the multicrab
 number_of_units=1 ## this is temporrt, will be changed by the multicrab
 
@@ -19,7 +19,7 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'treeMaker_16_17_cfg.py'
 config.JobType.pyCfgParams = ['runOnMC=True','runOn2017=True']
 
-config.JobType.inputFiles = ['../MetaData/data/DNN_models/breg_training_2017.pb',  ## this is for b-jet regression
+config.JobType.inputFiles = ['../MetaData/data/DNN_models/model-51.pb',  ## this is for b-jet regression
                              '../TreeMaker/data/BoostedSVDoubleCA15_withSubjet_v4.weights.xml'  ## this is for CA15 training, will be removed in next iteration
                              ]
 
@@ -34,12 +34,12 @@ config.Data.inputDBS = 'global'
 #config.Data.outputDatasetTag = config.General.requestName
 
 #config.Data.splitting = 'LumiBased'
-#config.Data.splitting = 'Automatic'
+config.Data.splitting = 'Automatic'
 #config.Data.splitting = 'FileBased'
 #config.Data.unitsPerJob = number_of_units
 
 config.Data.splitting = 'EventAwareLumiBased'
-config.Data.unitsPerJob = 50000
+#config.Data.unitsPerJob = 50000
 
 #config.Data.ignoreLocality = True
 config.JobType.allowUndistributedCMSSW=True
